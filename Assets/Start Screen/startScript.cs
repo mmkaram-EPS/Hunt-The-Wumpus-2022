eca54c11fed75df2b907ea3edb8585966d3b481b
plagality <93016934+plagality@users.noreply.github.com>
2022-03-24 15:08:44 -0700

    uh start screen looks nice woo

--- Assets/Start Screen/startScript.cs
+++ Assets/Start Screen/startScript.cs
@@ -2,29 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class startingGame : MonoBehaviour
+public class startScript : MonoBehaviour
 {
     public GameObject Exposition;
     public GameObject IntroText;
-    public GameObject beginScreen;
+    public GameObject BeginScreen;
     // Start is called before the first frame update
     void Start()
     {
         Exposition.SetActive(true);
         IntroText.SetActive(false);
-        beginScreen.SetActive(true);
+        BeginScreen.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Submit") && beginScreen.activeSelf)
+        if (Input.GetButtonDown("Submit") && BeginScreen.activeSelf) 
         {
-            beginScreen.SetActive(false);
-            beginScreen.SetActive(true);
+            BeginScreen.SetActive(false);
+            IntroText.SetActive(true);
         } else if (Input.GetButtonDown("Submit"))
         {
             Exposition.SetActive(false);
+            //roomLoader();
         }
     }
 }
