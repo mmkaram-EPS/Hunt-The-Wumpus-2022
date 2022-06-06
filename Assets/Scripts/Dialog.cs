ccf8990293d74d2bfb8284e9ff487d8318332fbf
ajosan25 <97420031+ajosan25@users.noreply.github.com>
2022-06-05 21:04:24 -0700

    Dialog box ok button. Upgraded to array of text as well

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -9,27 +9,50 @@ public class Dialog : MonoBehaviour
     public GameObject holder;
     public Player player;
 
-    public void StartText(string input)
+    bool ok = false;
+
+    void Start()
+    {
+        StartText(new string[] {"Hello", "I am robot"});
+    }
+
+    public void StartText(string[] input)
     {
         holder.SetActive(true);
         player.Freeze();
         StartCoroutine(LoadText(input));
     }
 
-    IEnumerator LoadText(string input)
+    IEnumerator LoadText(string[] input)
     {
-        // Reset the Text
-        dialogBox.SetText("");
-        // Kinda Smooth Anim thing
-        for (int i = 0; i < input.Length; i++)
+        // Keep doing dialogs until we are done with the array
+        for (int j = 0; j < input.Length; j++)
         {
-            dialogBox.text += input[i];
-            yield return new WaitForSeconds(0.1f);
+            // Reset the Text
+            dialogBox.SetText("");
+            // Kinda Smooth Anim thing
+            for (int i = 0; i < input[j].Length; i++)
+            {
+                dialogBox.text += input[j][i];
+                yield return new WaitForSeconds(0.1f);
+            }
+
+            // Wait until they press next
+            yield return new WaitUntil(() => ok);
+
+            // Set pressedNext back to false;
+            ok = false;
         }
+        
 
         // Unfreeze the player, reset the dialog box, disable the UI
         player.Freeze();
         dialogBox.SetText("");
         holder.SetActive(false);
+    }
+
+    public void PressOk()
+    {
+        ok = true;
     }
 }
