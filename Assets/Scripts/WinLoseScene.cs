0f024dd3331cf02d4dc713402a3288f03e527f12
Birdbrain6000 <38164449+Birdbrain6000@users.noreply.github.com>
2022-06-08 11:32:42 -0700

    SECRET NOTE??

--- Assets/Scripts/WinLoseScene.cs
+++ Assets/Scripts/WinLoseScene.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class WinLoseScene : MonoBehaviour
 {
     public string w = "Win";
     public TextMeshProUGUI text;
+    public NoteSpawner n;
+    public Notes no;
+    public GameObject noteGameObj;
+    public Text noteTextGameObj;
 
     void Start()
     {
+        n = GameObject.FindWithTag("NoteSpawner").GetComponent<NoteSpawner>();
+        no = GameObject.FindWithTag("Note").GetComponent<Notes>();
         text.text = "You " + w + " Score: " + GameObject.FindWithTag("GameManager").GetComponent<GameManager>().score.ToString();
     }
 
@@ -19,5 +26,10 @@ public class WinLoseScene : MonoBehaviour
         {
             Application.Quit();
         }
+        if (n.notesCollected >= 10){
+            noteGameObj.SetActive(true);
+            no.loadNotePublic(n.noteTextSecret, noteGameObj, noteTextGameObj);
+        }
+
     }
 }
